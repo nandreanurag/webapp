9adad627ff0b81955857e2e29aba17af3f341e30
nandreanurag <nandre.a@northeastern.edu>
2023-01-30 02:31:52 -0500

    junit 5 changes

--- src/test/java/edu/neu/coe/csye6225/webapp/WebappApplicationTests.java
+++ src/test/java/edu/neu/coe/csye6225/webapp/WebappApplicationTests.java
@@ -1,63 +1,20 @@
 package edu.neu.coe.csye6225.webapp;
 
-import org.assertj.core.api.Assertions;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = { WebappApplicationTests.class })
 @SpringBootTest
 class WebappApplicationTests {
-
-//	@Autowired
-//	private UserRepository userRepo;
-//	@Autowired
-//	private UserService userService;
-//	
-//	@Test
-//    void canAddStudent() {
-//        // given
-//	  User user = new User();
-//      user.setUsername("test.1998@gmail.com");
-//      user.setFirstName("Tester");
-//      user.setLastName("Junit");
-//      user.setPassword("123456");
-//
-//        // when
-//      try {
-//		userService.createUser(user);
-//	} catch (UserExistException e) {
-//		// TODO Auto-generated catch block
-//		e.printStackTrace();
-//	}
-//
-//        // then
-//        ArgumentCaptor<User> userArgumentCaptor =
-//                ArgumentCaptor.forClass(User.class);
-//        verify(userRepo)
-//                .save(userArgumentCaptor.capture());
-//        User capturedStudent = userArgumentCaptor.getValue();
-//        assertThat(capturedStudent).isEqualTo(user);
-//    }
-
-//	
-//	@Test
-//    @Order(1)
-//    @Rollback(value = false)
-//    public void saveUserTest(){
-//      User user = new User();
-//      user.setUsername("test.1998@gmail.com");
-//      user.setFirstName("Tester");
-//      user.setLastName("Junit");
-//      user.setPassword("123456");
-//      User userObj = userRepo.save(user);
-//	  System.out.println(userObj);
-//	  Assertions.assertThat(userObj.getId()).isGreaterThan(0);
-//    }
 	
 	@Test
     public void Test() {
-		Assertions.assertThat(1).isGreaterThan(0);
+		assertTrue("Hello".equals("Hello"));
 	}
 	
 }
